58565802919751ba09bdb09174b155478c53cf3b
joachim.kainz <joachim.kainz@c86e12c4-a0bc-11de-afa9-e300858c10cc>
2010-05-07 15:28:08 +0000

    added unit tests.
    git-svn-id: http://jolira-tools.googlecode.com/svn/guice/trunk@2511 c86e12c4-a0bc-11de-afa9-e300858c10cc

--- plugins/src/test/java/com/google/code/joliratools/plugins/PluginManagerTest.java
+++ plugins/src/test/java/com/google/code/joliratools/plugins/PluginManagerTest.java
@@ -3,22 +3,87 @@
  */
 package com.google.code.joliratools.plugins;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
+import java.util.Arrays;
+import java.util.Set;
 
 import org.junit.Test;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.TypeLiteral;
+import com.google.inject.internal.Sets;
+import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 /**
  * @author jfk
- * 
  */
 public class PluginManagerTest {
+    public static class MyTest {
+        @Inject
+        public MyTest(@Named("abc") final Set<String> abcde) {
+            assertEquals(setOf("A", "B", "C"), abcde);
+        }
+    }
+
+    private static <T> Set<T> setOf(final T... elements) {
+        final Set<T> result = Sets.newHashSet();
+        result.addAll(Arrays.asList(elements));
+        return result;
+    }
+
+    final TypeLiteral<Set<String>> setOfString = new TypeLiteral<Set<String>>() {
+    };
+
     @Test
     public void testMain() {
         final PluginManager mgr = new PluginManager();
         final Injector injector = mgr.getInjector();
 
         assertNotNull(injector);
+    }
+
+    @Test
+    public void testMultibinderAggregationForAnnotationInstance() {
+        final Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                Multibinder<String> multibinder = Multibinder.newSetBinder(binder(), String.class, Names.named("abc"));
+                multibinder.addBinding().toInstance("A");
+                multibinder.addBinding().toInstance("B");
+
+                multibinder = Multibinder.newSetBinder(binder(), String.class, Names.named("abc"));
+                multibinder.addBinding().toInstance("C");
+            }
+        });
+
+        final Set<String> abcde = injector.getInstance(Key.get(setOfString, Names.named("abc")));
+        assertEquals(setOf("A", "B", "C"), abcde);
+    }
+
+    @Test
+    public void testMultibinderInjectSet() {
+        final Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                Multibinder<String> multibinder = Multibinder.newSetBinder(binder(), String.class, Names.named("abc"));
+                multibinder.addBinding().toInstance("A");
+                multibinder.addBinding().toInstance("B");
+
+                multibinder = Multibinder.newSetBinder(binder(), String.class, Names.named("abc"));
+                multibinder.addBinding().toInstance("C");
+            }
+        });
+
+        final MyTest myTest = injector.getInstance(MyTest.class);
+
+        assertNotNull(myTest);
     }
 }
